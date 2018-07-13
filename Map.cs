dafd1718321560bc20ff08ff1ea1b165068b28e7
Reece Collett <reececollett@hotmail.co.uk>
2018-07-13 15:02:03 +0000

    Added OnMap method to Map class

--- Map.cs
+++ Map.cs
@@ -15,5 +15,28 @@ namespace TreehouseDefense
       Width = width;
       Height = height;
     }
+    
+    //Method that determines whether a given point is within the bounds of the map
+    
+    public bool OnMap(Point point)
+    {
+      
+      //This will return true if all of these conditions are true.
+        //NTS: Dont need to use an if statement for every time you need a boolean result
+      
+      bool inBounds = point.XCord >= 0 && point.XCord < Width && point.YCord >= 0 && point.YCord < Height;
+      
+      /* Another way to implement this is like this:
+      
+      bool inBounds = !(point.XCord < 0 || point.XCord >= width || point.YCord < 0 || point.YChord >= height);
+      
+      ! = NOT operator
+      || = OR operator
+      && = AND operator
+      
+      */
+      
+      return inBounds;
+    }
   }
 }
