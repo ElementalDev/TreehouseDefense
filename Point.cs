0cd00647cf548727d5a96e1ca5102bea75a6b84a
Reece Collett <reececollett@hotmail.co.uk>
2018-07-13 14:19:39 +0000

    Added point class, added x and y cords to it to determine points on the grid

---
+++ Point.cs
@@ -0,0 +1,14 @@
+namespace TreehouseDefense
+{
+  class Point
+  {
+    public readonly int XCord;
+    public readonly int YCord;
+    
+    public Point(int x, int y)
+    {
+      XCord = x;
+      YCord = y;
+    }
+  }
+}
