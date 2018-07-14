870087e765388f88252e4ae939494d18651dbdc9
Reece Collett <reececollett@hotmail.co.uk>
2018-07-14 15:03:22 +0000

    Added DistanceTo method to Points class to find distance between points using cartesian distance formula

--- Game.cs
+++ Game.cs
@@ -14,18 +14,7 @@ namespace TreehouseDefense
           //Creating a point
           Point point = new Point(4, 2);
           
-          //Storing whether the point is on the map
-          bool isOnMap = map.OnMap(point);
-          
-          //Print result to console
-          
-          Console.WriteLine(isOnMap);
-          
-          //Creating a new point (testing to see if it will go out of bounds)
-          point = new Point(8, 5);
-          isOnMap = map.OnMap(point);
-          
-          Console.WriteLine(isOnMap);
+          Console.WriteLine(point.DistanceTo(5, 5));
         }
     }
 }
--- Point.cs
+++ Point.cs
@@ -1,14 +1,45 @@
+using System;
+
 namespace TreehouseDefense
 {
   class Point
   {
-    public readonly int XCord;
-    public readonly int YCord;
+    public readonly int XCoord;
+    public readonly int YCoord;
     
     public Point(int x, int y)
     {
-      XCord = x;
-      YCord = y;
+      XCoord = x;
+      YCoord = y;
+    }
+    
+    public int DistanceTo(int x, int y)
+    {
+      //Uses Cartesian Distance Equation with 2 Parameters
+      
+      //Find the difference between the point we are at and the one we want to reach
+      
+      int xDiff = XCoord - x;
+      int yDiff = YCoord - y;
+      
+      //Square the differences
+      
+      int xDiffSquared = xDiff * xDiff;
+      int yDiffSquared = yDiff * yDiff;
+      
+      //Add the squared differences then sqrt them
+      
+      return (int) Math.Sqrt(xDiffSquared + yDiffSquared);
+      
+      /* This can be written in a single line like this:
+      
+        return (int) Math.Sqrt(Math.Pow(XCoord - x, 2) + Math.Pow(YCoord - y, 2));
+        
+        ADV:
+        - Less Variables: Less likely to type wrong one in wrong place
+        
+        DISADV:
+        -No broken up and less readable: Harder to think about
     }
   }
 }
