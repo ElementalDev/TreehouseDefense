36158b69c06eec61b717d1f1ec4eff3580dd4f40
Reece Collett <reececollett@hotmail.co.uk>
2018-07-16 15:19:59 +0000

    Added conditions to constructor of maplocation to throw an exception if
    q

--- Game.cs
+++ Game.cs
@@ -11,21 +11,14 @@ namespace TreehouseDefense
           
           Map map = new Map(8, 5);
           
-          //Creating a point. Still works as MapLocation Inherits point
-          Point mL = new MapLocation(4, 2);
-         
-          Point p = mL;
-          
-          map.OnMap(new MapLocation(0, 0));
-          
-          //Therefore being able to use its public methods
-          Console.WriteLine(mL.DistanceTo(5, 5));
-          
-          Console.WriteLine(mL is MapLocation);
-          Console.WriteLine(mL is Point);
-          
-          Point point = new Point(0, 0);
-          Console.WriteLine(point is MapLocation);
+          try
+          {
+            MapLocation mapL = new MapLocation(20, 20, map);
+          }
+          catch(Exception)
+          {
+            Console.WriteLine("That map location is not on the map!");
+          }
         }
     }
 }
