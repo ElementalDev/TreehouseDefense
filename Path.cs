5d07161f299b58b40ea181f347f3a7f509169bf5
Reece Collett <reececollett@hotmail.co.uk>
2018-07-17 15:06:49 +0000

    Added a constructor to Path class and a way to get the location of enemies using an array

--- Path.cs
+++ Path.cs
@@ -3,5 +3,23 @@ namespace TreehouseDefense
   class Path
   {
     
+    private readonly MapLocation[] _path;
+    
+    public Path(MapLocation[] path)
+    {
+      _path = path;
+    }
+    
+    public GetLocationAt(int pathStep)
+    {
+      try
+      {
+        return _path[pathStep];
+      }
+      catch(System.IndexOutOfRangeException)
+      {
+        return null;
+      }
+    }
   }
 }
